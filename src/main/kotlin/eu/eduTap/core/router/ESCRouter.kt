c0cff105d47283bb41132d89d4701734154682d2
Nejc Planer <nejcplaner@gmail.com>
2025-06-24 18:26:38 +0200

    ESC router wrapper (#2)
    * MVP implementation
    
    * Add docs
    
    * Cleanup
    
    * Docs

---
+++ src/main/kotlin/eu/eduTap/core/router/ESCRouter.kt
@@ -0,0 +1,45 @@
+package eu.eduTap.core.router
+
+import io.ktor.client.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+
+/**
+ * ESCRouter is a client for interacting with the European Student Card (ESC) Router API.
+ *
+ * It provides methods to manage persons and cards, including creating, updating, retrieving, and deleting records,
+ * as well as fetching card QR codes. The client handles authentication and serialization for API requests and responses.
+ *
+ * In case of an API error, it throws an [ESCRouterApiException] with the status code and error message.
+ *
+ * Links to the API documentation:
+ * - Router: https://router.europeanstudentcard.eu/esc-rest/swagger-ui/index.html
+ * - Sandbox: https://sandbox.europeanstudentcard.eu/esc-rest/swagger-ui/index.html
+ *
+ * @param apiToken The API token used for Bearer authentication.
+ * @param baseUrl The base URL of the ESC Router API (default: https://router.europeanstudentcard.eu/, sandbox: https://sandbox.europeanstudentcard.eu/).
+ */
+class ESCRouter(
+  apiToken: String,
+  baseUrl: String = "https://router.europeanstudentcard.eu/",
+) {
+  private val apiUrl = "${baseUrl}esc-rest/api/v2/"
+
+  private val httpClient = HttpClient {
+    install(Auth) {
+      bearer {
+        loadTokens {
+          BearerTokens(accessToken = apiToken, refreshToken = null)
+        }
+      }
+    }
+    install(ContentNegotiation) {
+      json()
+    }
+  }
+
+  val persons = PersonsApi(httpClient, apiUrl)
+  val cards = CardsApi(httpClient, apiUrl)
+}
---
+++ src/main/kotlin/eu/eduTap/core/router/PersonsApi.kt
@@ -0,0 +1,95 @@
+package eu.eduTap.core.router
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+
+/***
+ * ESI Format:
+ *      - For countries with a nation wide scope student ID PREFIX:CN:STUDENTCODE
+ *      - For countries with a HEI wide scope student ID PREFIX:SchacHomeOrganization:STUDENTCODE
+ *    CN - Country code of the institution (ISO 3166-1 norm)
+ *    PREFIX - urn:schac:personalUniqueCode:int:esi
+ *    SchacHomeOrganization is the domain of the Higher Education Institution
+ *    STUDENTCODE - Student unique code in the HEI where he is enrolled. If the country have national/regional identity code, this code will be national/regional wide, otherwise it will be the home institution own identifier.
+ */
+class PersonsApi(httpClient: HttpClient, apiUrl: String) : ESCApi(httpClient, apiUrl) {
+  private val personApiUrl = "${apiUrl}persons"
+
+  suspend fun get(esi: String): Person {
+    return makeRequest { httpClient.get("${personApiUrl}/${esi}") }
+  }
+
+  suspend fun create(
+    esi: String,
+    fullName: String,
+    organisationIdentifier: String,
+    academicLevel: AcademicLevel? = null,
+    email: String? = null,
+    phone: String? = null,
+    fax: String? = null,
+  ): Person {
+    return makeRequest {
+      httpClient.post(personApiUrl) {
+        contentType(ContentType.Application.Json)
+
+        setBody(
+          PersonUpdate(
+            fullName = fullName,
+            identifier = esi,
+            identifierCode = "ESI",
+            personOrganisationUpdateViews = listOf(
+              PersonUpdate.PersonOrganisationUpdate(
+                academicLevel = academicLevel,
+                email = email,
+                fax = fax,
+                organisationIdentifier = organisationIdentifier,
+                phone = phone,
+              )
+            )
+          )
+        )
+      }
+    }
+  }
+
+  suspend fun update(
+    esi: String,
+    fullName: String,
+    organisationIdentifier: String,
+    academicLevel: AcademicLevel? = null,
+    email: String? = null,
+    phone: String? = null,
+    fax: String? = null,
+  ): Person {
+    return makeRequest {
+      httpClient.put("${personApiUrl}/${esi}") {
+        contentType(ContentType.Application.Json)
+
+        setBody(
+          PersonUpdate(
+            fullName = fullName,
+            identifier = esi,
+            identifierCode = "ESI",
+            personOrganisationUpdateViews = listOf(
+              PersonUpdate.PersonOrganisationUpdate(
+                academicLevel = academicLevel,
+                email = email,
+                fax = fax,
+                organisationIdentifier = organisationIdentifier,
+                phone = phone,
+              )
+            )
+          )
+        )
+      }.body()
+    }
+  }
+
+  suspend fun delete(esi: String) {
+    makeRequest<String> {
+      httpClient.delete("${personApiUrl}/${esi}")
+    }
+  }
+}
