906d5c7ee453246fb9aa1e349f2787fd64e364fe
Fabian Schedler <fabian.schedler@studo.com>
2025-10-15 11:41:49 +0200

    Add wrapper for HttpResponse to LoggerContext

--- src/main/kotlin/eu/eduTap/core/router/ContextLoggerPlugin.kt
+++ src/main/kotlin/eu/eduTap/core/router/ContextLoggerPlugin.kt
@@ -5,7 +5,11 @@
 package eu.eduTap.core.router
 
 import io.ktor.client.plugins.api.*
+import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.util.*
 import kotlinx.coroutines.currentCoroutineContext
 import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
@@ -13,7 +17,7 @@ import kotlin.coroutines.CoroutineContext
 internal val ContextLogger = createClientPlugin("ContextLogger") {
   onResponse { response ->
     val logger = currentCoroutineContext()[LoggerContext.Key] ?: return@onResponse
-    logger.logResponse(response)
+    logger.logResponse(LoggerResponse(response))
   }
 }
 
@@ -23,8 +27,8 @@ internal val ContextLogger = createClientPlugin("ContextLogger") {
  *
  * ```kotlin
  * val logger = object : LoggerContext() {
- *   override suspend fun logResponse(response: HttpResponse) {
- *     println("${response.request.method.value} ${response.request.url} -> ${response.status.value}")
+ *   override suspend fun logResponse(response: LoggerResponse) {
+ *     println("${response.method} ${response.url} -> ${response.statusCode}")
  *   }
  * }
  *
@@ -34,7 +38,31 @@ internal val ContextLogger = createClientPlugin("ContextLogger") {
  * ```
  */
 abstract class LoggerContext : AbstractCoroutineContextElement(Key) {
-  abstract suspend fun logResponse(response: HttpResponse)
+  abstract suspend fun logResponse(response: LoggerResponse)
 
   companion object Key : CoroutineContext.Key<LoggerContext>
+}
+
+/**
+ * Wrapper around [HttpResponse] to provide access to common properties as basic types without needing to import Ktor.
+ */
+class LoggerResponse internal constructor(private val response: HttpResponse) {
+  val statusCode: Int get() = response.status.value
+  val method: String get() = response.request.method.value
+  val url: String get() = response.request.url.toString()
+  val headers: Map<String, List<String>> get() = response.headers.toMap()
+
+  suspend fun responseBodyAsText(): String = response.bodyAsText()
+
+  /** Only supports text and form data bodies. Other body types will return "Body omitted". */
+  fun requestBodyAsText(): String? = when (val body = response.request.content.unwrap()) {
+    is OutgoingContent.NoContent -> null
+    is TextContent -> body.text
+    is FormDataContent -> body.formData.formUrlEncode()
+    is OutgoingContent.ContentWrapper -> throw IllegalStateException("Unwrapped ContentWrapper")
+    else -> "Body omitted${body.contentLength?.let { " ($it bytes)" }.orEmpty()}"
+  }
+
+  private fun OutgoingContent.unwrap(): OutgoingContent =
+    if (this is OutgoingContent.ContentWrapper) delegate().unwrap() else this
 }
